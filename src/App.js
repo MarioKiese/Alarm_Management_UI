20b25b8dc401a627f8d396ec269414c17fd3c898
Mario Kiese <mali.mk73@googlemail.com>
2019-03-21 16:40:25 +0100

    Table added, Buttons added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar from './components/ButtonAppBar.js';
+import Table from './components/Table';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        main: '#24959e',
+    },
+    secondary: {
+        main: '#87bc43',
+    },
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <ButtonAppBar />
+        <Table />
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/CustomButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+// We can inject some CSS into the DOM.
+const styles = {
+  root: {
+    marginRight: '5px',
+    marginLeft: '25px',
+  },
+};
+
+function ClassNames(props) {
+  const { classes, children, className, ...other } = props;
+
+  return (
+    <Button className={classNames(classes.root, className)} {...other}>
+      {children || 'class names'}
+    </Button>
+  );
+}
+
+ClassNames.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+};
+
+export default withStyles(styles)(ClassNames);
