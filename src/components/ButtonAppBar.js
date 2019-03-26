4eba49638e3fbaab3babea04377c8db70dc29496
Mario Kiese <mali.mk73@googlemail.com>
2019-03-26 15:26:42 +0100

    Removed login button from AppBar

--- src/components/ButtonAppBar.js
+++ src/components/ButtonAppBar.js
@@ -4,7 +4,6 @@ import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 
@@ -38,7 +37,6 @@ function ButtonAppBar(props) {
           <Typography variant="h6" color="inherit" className={classes.grow}>
             Alarmmanagement Client
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
